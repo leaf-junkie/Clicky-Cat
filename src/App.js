62c019137c66b9244aeea8fdb62e81356c5e64f4
leaf-junkie <ragsdale.jar@gmail.com>
2019-05-05 11:41:39 -0400

    clicking an image successfully triggers shuffle function. need to work on winning and losing logic

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
       <div>
         <Jumbotron/>
         <Score/>
-        {/* <Container /> */}
+        <Container/>
         <GameLogic/>
         <Footer/>
       </div>
--- src/components/Cat/index.js
+++ src/components/Cat/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { clickCat } from "../GameLogic/GameLogic"
 
 class Cat extends React.Component {
     render() {
@@ -8,7 +9,12 @@ class Cat extends React.Component {
                     src={this.props.image} 
                     className="clicked" 
                     alt="cat"
-                    style={{ maxWidth: 200, margin: 8 }}
+                    style={{ 
+                        maxWidth: 200, 
+                        margin: 8,
+                        borderRadius: 25 
+                    }}
+                    onClick={this.props.clickCat}
                 >
                 </img>
             </div>
